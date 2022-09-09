19100471140ecc2813aeb4309a5c7139e0487eef
Silvana ALves <silvanarodriguesalvess@gmail.com>
2022-09-08 23:22:48 -0300

    Comentário C1

--- PrimeiroExemplo/OlaMundo.cs
+++ PrimeiroExemplo/OlaMundo.cs
@@ -7,18 +7,8 @@ namespace INF099
             Console.WriteLine("Olá Alunos INF-099 !!!!");
 			Console.WriteLine("BOA NOITE");
             Console.WriteLine("Alterações com VS Code");
-            Console.WriteLine("Alterações feitas por mim");
-            Console.WriteLine("Alterações feitas por mim 2");
-            // C2
-            // C5
-            // C6
-            // C3
-            // C8
-            // C9 
-            // C4
-            // C10
-            // Conflito
-            // Conflito Main
+            
+        
         }
     }
 }
